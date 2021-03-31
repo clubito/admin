a7a4c158137fc69bbd5fd6facd2d50e4a76957c0
Tung Dao <dvtung98@gmail.com>
2021-03-30 23:02:57 -0400

    implement home page

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ClubService } from 'src/app/services/club.service';
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  private subscriptions: Subscription[] = [];
+  private clubsNum: number;
+  private requestsNum: number;
+  constructor(private clubService: ClubService) { 
+    this.requestsNum = this.clubService.getClubRequests().length;
+    this.clubsNum = this.clubService.getClubApproved().length;
+  }
 
   ngOnInit() {
+    this.subscriptions.push(this.clubService.getClubRequestsEmitter().subscribe(response => {
+      this.requestsNum = response.length;
+    }))
+    this.subscriptions.push(this.clubService.getClubApprovedEmitter().subscribe(response => {
+      this.clubsNum = response.length;
+    }))
+    this.clubService.getAllClubs();
   }
 
 }
