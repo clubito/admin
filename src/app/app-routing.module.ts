91a02493e23d2949daf3e0a545f088715c4d8ffa
Tung Dao <dvtung98@gmail.com>
2021-04-29 23:45:36 -0400

    Add setting component template, change navbar to include setting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AlluserComponent } from './components/alluser/alluser.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { ClubrequestComponent } from './components/clubrequest/clubrequest.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { SettingsComponent } from './components/settings/settings.component';
 import { SingleclubComponent } from './components/singleclub/singleclub.component';
 import { SingleuserComponent } from './components/singleuser/singleuser.component';
 import { AuthGuard } from './guards/auth.guard';
@@ -16,7 +17,8 @@ const routes: Routes = [
   {path: "clubs/requests", component: ClubrequestComponent, canActivate: [AuthGuard]},
   {path: "clubs/:name", component: SingleclubComponent, canActivate: [AuthGuard]},
   {path: "users", component: AlluserComponent, canActivate: [AuthGuard]},
-  {path: "users/:id", component: SingleuserComponent, canActivate: [AuthGuard]}
+  {path: "users/:id", component: SingleuserComponent, canActivate: [AuthGuard]},
+  {path: "settings", component: SettingsComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { ClubrequestComponent } from './components/clubrequest/clubrequest.component';
 import { AlluserComponent } from './components/alluser/alluser.component';
 import { SingleuserComponent } from './components/singleuser/singleuser.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { SingleuserComponent } from './components/singleuser/singleuser.componen
     ClubrequestComponent,
     AlluserComponent,
     SingleuserComponent,
+    SettingsComponent,
   ],
   imports: [
     BrowserModule,
